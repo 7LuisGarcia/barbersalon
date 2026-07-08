42365a9bfb0d0c10021f540bd081bbe63a250a28
7LuisGarcia <garcia062785@gmail.com>
2026-07-08 13:34:05 -0700

    Update English and Spanish translations

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,11 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),  # enables the set_language view
+    path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
 ]
 
 urlpatterns += i18n_patterns(
-    path('', include('shop.urls', namespace='shop')),
+    path("", include("shop.urls", namespace="shop")),
+    prefix_default_language=False,
 )
